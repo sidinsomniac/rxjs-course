071f4ae372cd511a3d2fcf9be1d3fc2c08a1f956
sidinsomniac <chatterjeesid1993@gmail.com>
2020-11-28 13:29:24 +0530

    Use imperative design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
 
 
 @Component({
@@ -11,14 +12,24 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    public beginnerCourses: Course[];
+    public advancedCourses: Course[];
 
-    constructor() {
+
+    constructor(private http: HttpClient) {
 
     }
 
     ngOnInit() {
 
-
+        this.http.get('/api/courses')
+            .pipe(
+                map((payload: { payload: Course[]; }) => Object.values(payload.payload))
+            )
+            .subscribe((courses: Course[]) => {
+                this.beginnerCourses = courses.filter((course: Course) => course.category === "BEGINNER");
+                this.advancedCourses = courses.filter((course: Course) => course.category === "ADVANCED");
+            });
 
     }
 
